691bce2e74d241bfc83edc0dce8d5b56efb012c5
Robisho <bigsten@list.ru>
2024-02-02 19:28:53 +0800

    Решение task_3.1.1

---
+++ src/main/java/com/itm/task8/dao/UserDaoImpl.java
@@ -0,0 +1,46 @@
+package com.itm.task8.dao;
+
+
+import com.itm.task8.model.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void saveUser(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        String hql = "delete from User u where u.id = :id";
+        entityManager.createQuery(hql)
+                .setParameter("id", id)
+                .executeUpdate();
+    }
+
+    @Override
+    public void updateUser(User user) {
+        entityManager.merge(user);
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        Query query = entityManager.createQuery("from User", User.class);
+        return query.getResultList();
+    }
+}
---
+++ src/main/java/com/itm/task8/service/UserServiceImpl.java
@@ -0,0 +1,48 @@
+package com.itm.task8.service;
+
+import com.itm.task8.dao.UserDao;
+import com.itm.task8.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserDao userDao;
+
+    @Autowired
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userDao.getUserById(id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @Override
+    @Transactional
+    public void saveUser(User user) {
+        userDao.saveUser(user);
+    }
+
+    @Override
+    @Transactional
+    public void deleteUser(Long id) {
+        userDao.deleteUser(id);
+    }
+
+    @Override
+    @Transactional
+    public void updateUser(User user) {
+        userDao.updateUser(user);
+    }
+}
